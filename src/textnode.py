410bf913747a2455690b5ce2d316c485f102ca7d
Gareth Macdonald <macdonald.gareth@gmail.com>
2025-07-29 10:28:20 +0200

    Add text enum and text node classes

---
+++ src/textnode.py
@@ -0,0 +1,29 @@
+from enum import Enum
+
+
+class TextType(Enum):
+    TEXT = "text"
+    BOLD = "bold"
+    ITALIC = "italic"
+    LINK = "link"
+
+
+class NodeType:
+    def __init__(self, text, text_type, url):
+        self.text = text
+        self.text_type = text_type
+        self.url = url
+
+    def __eq__(self, node):
+        try:
+            return (
+                self.text == node.text
+                and self.text_type == node.text_type
+                and self.url == node.url
+            )
+        except AttributeError:
+            print("ERROR: node '{node}' is not a known text node")
+            return False
+
+    def __repr__(self):
+        return f"TextNode({self.text}, {self.text_type}, {self.url})"
